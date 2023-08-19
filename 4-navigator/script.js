cabef043e7cc4188d9dd15b657ed8452fc976b72
ViacheslavSave <nikolya-ivanov-9191@mail.ru>
2023-08-19 15:13:02 +0300

    navigator

---
+++ 4-navigator/script.js
@@ -0,0 +1,7 @@
+const addressLat = 10;
+const addressLong = 14;
+const positionLat = 55;
+const positionLong = 70;
+
+const res = Math.sqrt(Math.pow((addressLat - addressLong), 2) + Math.pow((positionLat - positionLong), 2))
+console.log(res);
