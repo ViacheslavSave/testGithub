ef0d42725164fca261f7092d0e43cff56d5bc417
ViacheslavSave <nikolya-ivanov-9191@mail.ru>
2023-09-08 18:43:04 +0300

    Изменения после код ревью 2

--- 11-date-array/script.js
+++ 11-date-array/script.js
@@ -1,35 +1,37 @@
-function checkDate(arrStr) {
-	const res = [];
-	for (const strDate of arrStr) {
-		for (const variant of ["/", "-"]) {
-			if (!(strDate[2] === variant && strDate[5] === variant)) {
-				continue;
-			}
+const LONG_MONTH_ARRAY = [1, 3, 5, 7, 8, 10, 12];
 
-			const arrDate = strDate.split(variant);
-			if (arrDate.length !== 3 || arrDate[2] == 0 || arrDate.some((el) => isNaN(Number(el)))) {
-				continue;
-			}
+function getValidDate(arrDate) {
+	return arrDate.reduce((acc, date) => {
+		const res = checkDate(date);
+		return res ? [...acc, res] : acc;
+	}, []);
+}
 
-			if (variant === "/") {
-				[arrDate[0], arrDate[1]] = [arrDate[1], arrDate[0]];
-			}
+function checkDate(dateString) {
+	[month, day, year, checklength] = dateString.split("/");
 
-			const [day, manth, year] = arrDate.map((elem) => Number(elem));
-			let lastDay = 30;
-
-			if (!(manth > 0 && manth <= 12)) {
-				continue;
-			} else if (manth === 2) {
-				lastDay = !(year % 400) || (year % 100 && !(year % 4)) ? 29 : 28;
-			} else if ((manth <= 7 && manth % 2) || (manth > 7 && !(manth % 2))) {
-				lastDay = 31;
-			}
-
-			if (day > 0 && day <= lastDay) {
-				res.push(arrDate.join("-"));
-			}
-		}
+	if (!year) {
+		[day, month, year, checklength] = dateString.split("-");
 	}
-	return res;
+
+	switch (true) {
+		case checklength !== undefined:
+		case !(day > 0) || day.length !== 2:
+		case !(month > 0 && month <= 12) || month.length !== 2:
+		case !(year > 0) || year.length !== 4:
+			return null;
+	}
+
+	return day <= lastDay(Number(month), year) ? `${day}-${month}-${year}` : null;
+}
+
+function isLeapYear(year) {
+	return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+}
+
+function lastDay(month, year) {
+	if (month === 2) {
+		return isLeapYear(year) ? 29 : 28;
+	}
+	return LONG_MONTH_ARRAY.includes(month) ? 31 : 30;
 }
--- 12-luna/script.js
+++ 12-luna/script.js
@@ -2,20 +2,18 @@
 const card = "4561-2612-1234-5467";
 // const card = "2665-4565-9087-4563";
 
-
 function checkLuhn(card) {
 	cardNumber = card.replaceAll("-", "");
 	if (cardNumber.length !== 16 || !Number(cardNumber)) {
 		return false;
 	}
 
-	return !Boolean(
-		cardNumber.split("").reduce((acc, num, index) => {
-			if (!(index % 2)) {
-				const operationМultiply = num * 2;
-				return (acc += operationМultiply > 9 ? operationМultiply - 9 : operationМultiply);
-			}
-			return (acc += Number(num));
-		}, 0) % 10
-	);
+	const res = cardNumber.split("").reduce((acc, elem, index) => {
+		if (index % 2 === 0) {
+			const operationМultiply = elem * 2;
+			return (acc += operationМultiply > 9 ? operationМultiply - 9 : operationМultiply);
+		}
+		return (acc += Number(elem));
+	}, 0);
+	return res % 10 === 0;
 }
