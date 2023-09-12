085c9cbd8b321b358352b67a41e965029dc20b09
ViacheslavSave <nikolya-ivanov-9191@mail.ru>
2023-09-12 22:51:20 +0300

    Изменения после кодревью_2

--- 15-task-object/script2.js
+++ 15-task-object/script2.js
@@ -22,20 +22,48 @@ const ToDoList = {
 		return false;
 	},
 
-	editTask: function (obj) {
-		const editTaskCallback = (index) => (this.tasks[index] = { ...this.tasks[index], ...obj });
-		return this.result(obj.id, editTaskCallback);
+	editTask: function (id, updatedTask) {
+		const editTaskCallback = (index) => (this.tasks[index] = { ...this.tasks[index], ...updatedTask });
+		return this.result(id, editTaskCallback);
 	},
 
 	delTask: function (id) {
 		const delTaskCallback = (index) => this.tasks.splice(index, 1);
 		return this.result(id, delTaskCallback);
 	},
-
-	sortTasks: function (obj) {
+	//
+	//
+	//        Какой вариант лучше ?
+	//
+	//_____________________________________________________1
+	//
+	sortTasks: function (property, sortBy) {
 		return this.tasks.sort((a, b) => {
-			obj.sortBy === "increasing" && ([a, b] = [b, a]);
-			return b[obj.property] - a[obj.property];
+			sortBy === "increasing" && ([a, b] = [b, a]); //___________  проверка условия на каждой итерации и замена переменных если true
+			return b[property] - a[property];
 		});
+	},
+	//
+	//______________________________________________________2
+	//
+	//     одна проверка но практически дублированный код,
+	//
+	sortTasks__2: function (property, sortBy) {
+		switch (sortBy) {
+			case "increasing":
+				return this.tasks.sort((a, b) => b[property] - a[property]);
+			default:
+				return this.tasks.sort((a, b) => a[property] - b[property]);
+		}
+	},
+	//
+	//______________________________________________________3
+	//
+	//    или так
+	//
+	sortTasks__3: function (property, sortBy) {
+		return sortBy === "increasing"
+			? this.tasks.sort((a, b) => b[property] - a[property])
+			: this.tasks.sort((a, b) => a[property] - b[property]);
 	},
 };
