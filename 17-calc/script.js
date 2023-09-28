8030c9e2ba2fd05d9db964d8d6ab13388fbbdc7a
ViacheslavSave <nikolya-ivanov-9191@mail.ru>
2023-09-28 21:35:24 +0300

    изменения после кодревью 1

--- 17-calc/script.js
+++ 17-calc/script.js
@@ -1,23 +1,51 @@
 const form = document.forms.calculate;
+const resDiv = document.querySelector(".res");
 
-function validNum() {
-	return form.num_1.valueAsNumber && form.num_2.valueAsNumber;
+	for (const input of [form.num_1, form.num_2]) {
+		input.addEventListener("input", (e) => {
+			if (isNaN(e.target.value)) {
+				e.preventDefault(); // не работает, есть ли другой способ отменить действие ?
+
+				e.target.value = e.target.value.slice(0, -1);
+			}
+		});
+	}
+
+document.addEventListener("keydown", (e) => {
+	operation(e.key);
+});
+
+function checkValue() {
+	let error = false;
+	for (const input of [form.num_1, form.num_2]) {
+		if (input.value.length > 0) {
+			input.classList.remove("error");
+		} else {
+			input.classList.add("error");
+			error = true;
+		}
+	}
+	return error;
 }
 
-const resDiv = document.querySelector(".res");
-document.addEventListener("keydown", (e) => {
-	switch (e.key) {
+function operation(operator) {
+	const textError = "заполните все поля";
+	const num_1 = Number(form.num_1.value);
+	const num_2 = Number(form.num_2.value);
+
+	switch (operator) {
 		case "+":
-			resDiv.innerText = !validNum() ? "ошибка" : form.num_1.valueAsNumber + form.num_2.valueAsNumber;
+			resDiv.innerText = checkValue() ? textError : num_1 + num_2;
 			break;
 		case "-":
-			resDiv.innerText = !validNum() ? "ошибка" : form.num_1.valueAsNumber - form.num_2.valueAsNumber;
+			resDiv.innerText = checkValue() ? textError : num_1 - num_2;
 			break;
 		case "*":
-			resDiv.innerText = !validNum() ? "ошибка" : form.num_1.valueAsNumber * form.num_2.valueAsNumber;
+			resDiv.innerText = checkValue() ? textError : num_1 * num_2;
 			break;
 		case "/":
-			resDiv.innerText = !validNum() ? "ошибка" : form.num_1.valueAsNumber / form.num_2.valueAsNumber;
+			resDiv.innerText = checkValue() ? textError : num_1 / num_2;
 			break;
 	}
-});
+}
+
